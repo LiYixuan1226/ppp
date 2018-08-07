c8d2b43d27d9a88c3fac6091a77bbb9716fbb605
YixuanLi1226 <2305021l@student.gla.ac.uk>
2018-08-07 19:25:11 +0100

    pdf download 2

--- slr/slirm/filter/DownloadPDF.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-class Download(object):
-    def download(url, path):
-        url = '//www.jb51.net/test/demo.zip'
-        r = requests.get(url)
-        with open("path", "wb") as code:
-            code.write(r.content)
